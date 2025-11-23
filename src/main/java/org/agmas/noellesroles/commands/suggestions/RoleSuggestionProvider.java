373e13f6adf84eba6a5c170361f7050b58ffc43f
DayDarkTV <67245492+DayDarkTV@users.noreply.github.com>
2025-11-23 17:13:29 -0500

    Rewrite Commands
    - Separated commands into individual classes
    - Combined banRole and unbanRole commands
    - Improved command output text
    - Brought some command text into language file

---
+++ src/main/java/org/agmas/noellesroles/commands/suggestions/RoleSuggestionProvider.java
@@ -0,0 +1,29 @@
+package org.agmas.noellesroles.commands.suggestions;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import dev.doctor4t.trainmurdermystery.api.TMMRoles;
+import net.minecraft.command.CommandSource;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.util.Identifier;
+
+import java.util.concurrent.CompletableFuture;
+
+public class RoleSuggestionProvider implements SuggestionProvider<ServerCommandSource> {
+    @Override
+    public CompletableFuture<Suggestions> getSuggestions(CommandContext<ServerCommandSource> context, SuggestionsBuilder builder) throws CommandSyntaxException {
+        // suggest all roles
+        if (TMMRoles.ROLES.isEmpty()) return Suggestions.empty();
+
+        for (TMMRoles.Role role : TMMRoles.ROLES ) {
+            Identifier roleId = role.identifier();
+            if (roleId != null && CommandSource.shouldSuggest(builder.getRemaining(), roleId.getPath())) {
+                builder.suggest(roleId.getPath());
+            }
+        }
+        return builder.buildFuture();
+    }
+}
